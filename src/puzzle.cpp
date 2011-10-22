f5b9fc6317074390aca609eca5ee703a685d46ab
g.penet@orange.fr <g.penet@orange.fr@8b398ab4-6a91-4778-a256-5d1a0e9dd774>
2011-10-22 06:20:38 +0000

    changes to test TCHAIN change to become thread safe and to do immediate elimination if possible
    git-svn-id: svn://localhost/tmp/testgc/trunk@106 8b398ab4-6a91-4778-a256-5d1a0e9dd774

--- src/puzzle.cpp
+++ src/puzzle.cpp
@@ -39,6 +39,9 @@ const char *orig[]={"row ","column ","box "," "};
 const char *lc="ABCDEFGHI";
 const char *orig1="RCB ";
 
+//#include "puzle_safe.cpp"
+#include "puzzle_ww.cpp"
+
 GG::GG() {	// constructor
 	pg = g[0]; 
 	pg[81] = 0;
@@ -557,6 +560,7 @@ PUZZLE::PUZZLE() {
 	T81tc = T81C->t81;
 	yt.SetParent(this);
 	zpaires.SetParent(this);
+	tchain.SetParent(this,&EE);
 }
 
 
@@ -1055,6 +1059,9 @@ void PUZZLE::Chaining(int opt, int level, int base) {
 
 
 int PUZZLE::Rating_end(int next) {
+	   // if some immediate action done, answer yes imediatly)
+	   // nothing to do for filters
+	if(tchain.elims_done) 		return 1;
 	if(!tchain.IsOK(next))
 		return 0;
 	Step((SolvingTechnique)tchain.rating);
@@ -1633,7 +1640,7 @@ int PUZZLE::Traite(char * ze) {
 		if(stop_rating)
 			break; // in case we would exceed the TCAND limit
 		//=========================
-		tchain.Init();
+		tchain.NewCycle();
 
 		Step(AlignedPairExclusion);
 		if(rating_ir > 1)
@@ -4609,114 +4616,6 @@ return itret + nestedlength;}
 void PATH::PrintPathTags() {
 	zpln.PrintImply(pth,ipth);
 }
-
-
-void CHAIN::Load(USHORT cande) {
-	urem = puz.couprem;
-	cand=cande;
-}
-
-/*	Upper bound for chains is actually unbounded: the longer chain, the higher rating.
- */
-
-USHORT  steps[] = {4, 6, 8, 12, 16, 24, 32, 48, 64, 96, 128,
-            192, 256, 384, 512, 768, 1024, 1536, 2048, 3072, 4096, 6144, 8192};
-/* clean a candidate if the final rating is ok
-*/
-int CHAIN::Clean() // clear candidates
-     {if(Op.ot){EE.E("clean for UREM=");EE.Enl(urem);}
-      CANDIDATE cw=zpln.zp[cand];
-	  return T81t[cw.ig].Change(cw.ch);}
-/* accelerator for the search of loops
-   to be revised
-*/
-void TCHAIN::SetMaxLength( int bw)  
-       {base=bw;
-		if(bw<rating) 
-			{if(bw<75) maxlength=16; // same as no limit
-			 else maxlength=100;}
-		     // chexk why, but more creates a problem in bitfield
-		else if(bw>80 || bw==75) maxlength=100;// linked to CANDGO
-		else if(bw==rating )  maxlength=4;// equivalent to loop length 6
-		else {int ii=rating - bw; maxlength=steps[ii]/2+2;}
-       }
-/* same gaol in dynamic mode
-   to be revised as well
-   */
-USHORT TCHAIN::ParsingMini(USHORT basee) {if(rating>(basee+10)) return 200; //max
-                                   if(rating <=basee) return 6; // mini
-								   int ii=rating - basee; return(steps[ii]/2+2);}
-/* another function to be revised
-*/
-int TCHAIN::GetYlower(int lg) // limit for a shorter chain/loop
-       {int xl=2*(lg-1); //  equivalent length pure Y mode
-        if(xl<7 ) return 0; //nothing shorter can be found 
-		if(xl>32) return 0;// should never happen
-		xl-=2; // the length for index
-		for(int ii=0;ii<6;ii++) if(xl<steps[ii+1])	return steps[ii]/2+2;
-        return 0;} // shoul never happen
-/* new function to control performances
-   the target is the following
-   a chain has been found, what is the maximum length for a loop
-     having a lower rating
-  in 'X' 'Y' mode, the base is 0.1 lower
-  in 'XY' mode the base is the same
-  modexy is 1 if "XY" else it must be 0
-*/
- int TCHAIN::GetLimit(int lg,int modexy) // limit for that group
-       {lg-=2; // the length for index
-		for(int ii=0;ii<10;ii++) if(lg<=steps[ii])	
-			return (steps[ii]-modexy);
-        return 0;} // shoul never happen
-/* attempt to enter the table of pending eliminations.
-   compute the rating and return 0 if
-    . higher than current rating, 
-	. equal to current rating and 
-	   - table is full
-	   - or the cand is already in pending eliminations
-   if not
-   return the rating found
-*/
-int TCHAIN::GetRatingBase(USHORT bb,USHORT lengthe,USHORT cand)
-       { if(lengthe<2) return 0; // should not happen
-		  USHORT wrating=300,length=lengthe-2; int ii;
-          for(ii=0;ii<22;ii++) 
-	         if(length<= steps[ii]) {wrating=bb+ii; break;}
-          if(wrating>rating) return 0;
-		  if(wrating==rating )
-			  {if(ichain >=30)  return 0;
-		       for(int i=0;i<ichain;i++) if(chains[i].cand==cand) return 0;
-		      }
-		  return wrating;}
-
- /* Load after GetRating
-    check for safety the load is valid
-*/
-void TCHAIN::LoadChain(USHORT rat,char * lib,USHORT cand)
-{ if(rat>rating) return ;
-  if(rat<rating) {ichain=0;  rating=rat;}
-  if(ichain>=30) return;
-  if(Op.ot)
-           {EE.Enl();puz.PointK(); EE.Esp(); EE.Enl(lib);
-			EE.E(" load tchain rating=");EE.E(rat);
-		    EE.E(" elimination of ");zpln.Image(cand);
-			EE.Enl();}
-   chains[ichain++].Load(cand);
-}
-
-int TCHAIN::IsOK(USHORT x) {
-	int ir=((rating <= x) || (rating <= puz.ermax));
-	if(ir && Op.ot) {
-		EE.E(" sortie isok chaine x=");
-		EE.E(x); 
-		EE.E(" rating=");
-		EE.E(rating);  
-		EE.E(" ermax==");
-		EE.Enl(puz.ermax); }
-	return ir;
-	//{return ((rating <= x ) || (rating <= Op.ermax));}
-}
-
 
 
 
---
+++ src/puzzle_ww.cpp
@@ -0,0 +1,140 @@
+
+/*              <<<<<<  TCHAIN module   >>>>>>
+            handling all potential eliminations though chains
+
+			not yet fully thread safe
+			global variables still there
+
+*/
+
+
+
+
+
+/*	Upper bound for chains is actually unbounded: the longer chain, the higher rating.
+ */
+
+USHORT  steps[] = {4, 6, 8, 12, 16, 24, 32, 48, 64, 96, 128,
+            192, 256, 384, 512, 768, 1024, 1536, 2048, 3072, 4096, 6144, 8192};
+
+/* attach the instance to the puzzle
+*/
+
+void TCHAIN::SetParent(PUZZLE * parent,FLOG * fl){
+ parentpuz=parent;
+ EE=fl;}
+/* start a new cycle (if chains are needed)
+*/
+void TCHAIN::NewCycle() {
+	rating = 200;
+	ichain = 0;
+	elims_done=0;
+	cycle_elims.SetAll_0();
+	achieved_rating= parentpuz->ermax;
+	maxlength = 10;
+	}
+
+
+/* accelerator for the search of loops
+   to be revised
+*/
+void TCHAIN::SetMaxLength( int bw)  
+       {base=bw;
+		if(bw<rating) 
+			{if(bw<75) maxlength=16; // same as no limit
+			 else maxlength=100;}
+		     // chexk why, but more creates a problem in bitfield
+		else if(bw>80 || bw==75) maxlength=100;// linked to CANDGO
+		else if(bw==rating )  maxlength=4;// equivalent to loop length 6
+		else {int ii=rating - bw; maxlength=steps[ii]/2+2;}
+       }
+/* try to clear a candidate should always answer yes
+*/
+
+int TCHAIN::ClearChain(int ichain){ // clear candidates
+ if(Op.ot){EE->E("clean for UREM=");EE->Enl(chains[ichain].urem);}
+ CANDIDATE cw=zpln.zp[chains[ichain].cand];
+ return T81t[cw.ig].Change(cw.ch);
+}
+
+int TCHAIN::ClearImmediate(USHORT cand){ // clear candidates
+ if(cycle_elims.On(cand)) return 0;
+ if(Op.ot){EE->Enl("immediate elimination through chain for candidate ");
+           zpln.Image(cand);
+            EE->Enl();
+ }
+ cycle_elims.Set(cand);
+ CANDIDATE cw=zpln.zp[cand];
+ int ir=T81t[cw.ig].Change(cw.ch); // should always answer yes
+ if(ir) elims_done=1;   // but keep protected against loops
+ return ir;
+}
+
+int TCHAIN::Clean() { // clear all pending candidates
+ int ir = elims_done;  //  answer is yes is immediate eliminations done
+ for(int i = 0; i < ichain; i++)
+ 		ir += ClearChain(i);
+ return ir;
+}
+
+
+
+/* attempt to enter the table of pending eliminations.
+   compute the rating and return 0 if
+    . higher than current rating, 
+	. equal to current rating and 
+	   - table is full
+	   - or the cand is already in pending eliminations
+   if not
+   return the rating found
+*/
+int TCHAIN::GetRatingBase(USHORT bb,USHORT lengthe,USHORT cand)
+       { if(lengthe<2) return 0; // should not happen
+		  USHORT wrating=300,length=lengthe-2; int ii;
+          for(ii=0;ii<22;ii++) 
+	         if(length<= steps[ii]) {wrating=bb+ii; break;}
+          if(wrating>rating) return 0;
+		  if(wrating==rating )
+			  {if(ichain >=30)  return 0;
+		       for(int i=0;i<ichain;i++) if(chains[i].cand==cand) return 0;
+		      }
+		  return wrating;}
+
+
+ /* Load after GetRating
+    check for safety that the load is valid
+
+	if possible, (rating lower than the achived one)
+	do immediate elimination
+*/
+void TCHAIN::LoadChain(USHORT rat,char * lib,USHORT cand)
+{ if(rat>rating) return ;
+  if(rat<=achieved_rating) { //then do it  
+	  ClearImmediate(cand);
+	  return;
+  }
+  if(rat<rating) {ichain=0;  rating=rat;}
+  if(ichain>=30) return;
+  if(Op.ot)
+           {EE->Enl();puz.PointK(); EE->Esp(); EE->Enl(lib);
+			EE->E(" load tchain rating=");EE->E(rat);
+		    EE->E(" elimination of ");zpln.Image(cand);
+			EE->Enl();}
+   chains[ichain].cand=cand;
+   chains[ichain++].urem=parentpuz->couprem;
+}
+
+
+int TCHAIN::IsOK(USHORT x) {
+	int ir=((rating <= x) );
+	if(ir && Op.ot) {
+		EE->E(" sortie isok chaine x=");
+		EE->E(x); 
+		EE->E(" rating=");
+		EE->E(rating);  
+		EE->E(" ermax==");
+		EE->Enl(puz.ermax); }
+	return ir;
+	//{return ((rating <= x ) || (rating <= Op.ermax));}
+}
+
